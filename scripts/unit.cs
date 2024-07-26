8d9d5d8174d8ce89ffd79bd7a03bd05726106f2d
Konstantin Koch <korsarnek@googlemail.com>
2024-07-26 15:58:54 +0200

    - made cardboard boxes to be kicked around - improved path finding - fixed street grid not overlapping anymore - adjusted player collision box - made music play randomly - created a separate music bus - when a unit can't reach a target, but has a move command there, it teleports after a timeout

--- scripts/unit.cs
+++ scripts/unit.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Linq;
 
 public partial class unit : CharacterBody3D
 {
@@ -15,6 +16,8 @@ public partial class unit : CharacterBody3D
 	private int _remainingActionPoints = 0;
 	private NavigationAgent3D? _navigationAgent;
 	private AnimationPlayer? _animationPlayer;
+	private Timer? _timer;
+	private bool _timerStarted = false;
 
 	[Signal]
 	public delegate void ReachedTargetEventHandler();
@@ -33,22 +36,62 @@ public partial class unit : CharacterBody3D
 		_animationPlayer = (AnimationPlayer)FindChild("AnimationPlayer");
 		var animation = _animationPlayer.GetAnimation("running");
 		animation.LoopMode = Animation.LoopModeEnum.Linear;
-		_navigationAgent.NavigationFinished += () => {
-			_animationPlayer!.Stop();
-			EmitSignal(SignalName.ReachedTarget);
-		};
+		_navigationAgent.NavigationFinished += _TargetReached;
+	}
+
+	private void _TargetReached()
+	{
+		_animationPlayer!.Stop();
+		EmitSignal(SignalName.ReachedTarget);
+		if (_timerStarted)
+		{
+			_timer!.Stop();
+			_timerStarted = false;
+		}
+	}
+
+	private void _TimeoutReached()
+	{
+		//Teleport to target location if we are stuck.
+		Position = _navigationAgent!.TargetPosition;
+		_TargetReached();
 	}
 
     public override void _PhysicsProcess(double delta)
     {
 		if (_navigationAgent!.IsNavigationFinished())
 			return;
+		else if (!_timerStarted)
+		{
+			if (_timer is null)
+			{
+                _timer = new Timer
+                {
+                    OneShot = true,
+					Autostart = true,
+                };
+				_timer.Timeout += _TimeoutReached;
+                AddChild(_timer);
+			}
+			var length = _navigationAgent.GetCurrentNavigationResult().Path.Select(v => v.Length()).Sum();
+			_timer.Start(length / MovementSpeed * 1.25); //25% buffer 
+			_timerStarted = true;
+		}
 
 		var current = GlobalPosition;
 		var next = _navigationAgent.GetNextPathPosition();
+		var velocity = current.DirectionTo(next) * MovementSpeed;
 
-		Velocity = current.DirectionTo(next) * MovementSpeed;
+		Velocity = velocity;
 		LookAt(next);
+		// Kick objects around if we are colliding.
+		var collision = MoveAndCollide(velocity * (float)delta, testOnly: true);
+		if (collision?.GetCollisionCount() > 0 && collision.GetCollider() is RigidBody3D body)
+		{
+			var direction = collision.GetNormal();
+			var position = collision.GetPosition();
+			body.ApplyForce(direction.Rotated(UpDirection, MathF.PI) * MovementSpeed, body.ToLocal(position));
+		}
 		MoveAndSlide();
     }
 
@@ -57,4 +100,16 @@ public partial class unit : CharacterBody3D
 	{
 		
 	}
+
+    protected override void Dispose(bool disposing)
+    {
+		if (disposing)
+		{
+			if (_timer is not null)
+				_timer.Timeout -= _TimeoutReached;
+			if (_navigationAgent is not null)
+				_navigationAgent.TargetReached -= _TargetReached;
+		}
+        base.Dispose(disposing);
+    }
 }
